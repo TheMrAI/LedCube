c161c48b4f6a0f6e8bb19053d49e53efc5723c6a
Istvan Davorin Acs <mr.istvan.acs@gmail.com>
2018-10-28 15:19:09 +0100

    Extend sine wave to four layers

--- Driver/Driver.ino
+++ Driver/Driver.ino
@@ -1,28 +1,45 @@
 #include "math.h"
 #include "Tlc5940.h"
-#include "tlc_fades.h"
 
-int layer0 = 0;
-int layer1 = 1;
-int layer2 = 2;
-int layer3 = 3;
+int layer_pins[] = {4, 5, 6 ,7};
 
-void setup() {
-  Tlc.init();
-  Serial.begin(9600);
+void prime_layer(int layer_id)
+{
+  for (int i = 0; i < 4; ++i)
+  {
+    if (i == layer_id)
+    {
+      analogWrite(layer_pins[i], 0); // ON
+    }
+    else
+    {
+      analogWrite(layer_pins[i], 255); // OFF  
+    }
+  }
 }
 
-void loop() {
+void setup() 
+{
+  Tlc.init();
+}
+
+void loop() 
+{
+  int maxBrightness = 2047; // 4095
   int duration = 1000;
   double step_size = (2*PI / 16);
   for (double sine_state = 0.0; sine_state < 2*PI; sine_state += step_size)
   {
-   for (int i = 0; i < 16; ++i)
-   {
-      int brightness = (sin(sine_state + i*step_size)+1) * (4095/2);
-      Tlc.set(i, brightness);
-   }
-   Tlc.update();
-   delay(duration / 16);
+    for (int layer = 0; layer < 4; ++layer)
+    {
+      prime_layer(layer);
+      for (int i = 0; i < 16; ++i)
+      {
+        int brightness = (sin(sine_state + i*step_size)+1) * (maxBrightness/2);
+        Tlc.set(i, brightness);
+      }
+      Tlc.update();
+    }
+    delay(duration / 16);
   }
 }
